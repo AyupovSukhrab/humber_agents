5174dd595c919a3e81d35387c2f4cf96bc402b43
Sukhrab Ayupov <ayupov.sukhrab@gmail.com>
2018-04-22 21:45:11 -0400

    Update settings.py

--- humber/humber/settings.py
+++ humber/humber/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'b(!t8v$49811!(7#-arao^r-*_3&@(#n7oxwy-#kpv(ch$6w&x'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
